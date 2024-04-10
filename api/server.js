5acb7fdcefcbac4c0cf328dcb7694e18e524145c
zhangshuaining <1208505019@qq.com>
2024-04-10 21:12:36 +0800

    chore: edit db

--- api/server.js
+++ api/server.js
@@ -12,18 +12,18 @@ const server = jsonServer.create()
 // const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('jobs.json')
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
 server.use(router)
-server.listen(3000, () => {
+server.listen(8000, () => {
     console.log('JSON Server is running')
 })
 
